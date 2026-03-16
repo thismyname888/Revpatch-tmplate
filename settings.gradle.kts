0062309f0f8a1cef8ff90f62bb06bd8595286d9d
unrealperson666-Git95 <245884741+thismyname888@users.noreply.github.com>
2026-03-17 01:49:50 +0800

    Initialize patcher22

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+rootProject.name = "revanced-patches-template"
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        maven {
+            name = "githubPackages"
+            url = uri("https://maven.pkg.github.com/revanced/revanced-patches-template")
+            credentials(PasswordCredentials::class)
+        }
+    }
+}
+
+plugins {
+    id("app.revanced.patches") version "1.0.0-dev.8"
+}
